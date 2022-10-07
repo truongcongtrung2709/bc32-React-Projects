ff4f25f3e4066a4db6ed82cf1905bc6afcf9f100
trung <truongcongtrung2709@gmail.com>
2022-10-07 11:05:13 +0700

    init

--- src/ShoesStore/ShoesStore.jsx
+++ src/ShoesStore/ShoesStore.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
-
+import ShoesList from './ShoesList'
+import ShoesCart from './ShoesCart'
 export default class ShoeStore extends Component {
   render() {
     return (
-      <div>ShoeStore</div>
+      <div>
+        <ShoesList/>
+        <ShoesCart/>
+      </div>
     )
   }
 }
